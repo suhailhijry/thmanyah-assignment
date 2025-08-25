4304ece533981367fe6901e5d1f369492b9adc75
achilles <suhailhijry@gmail.com>
2025-08-25 19:03:40 +0300

    use fastify

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { GatewayModule } from './gateway.module';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(GatewayModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.create<NestFastifyApplication>(GatewayModule, new FastifyAdapter({ logger: true }));
+  await app.listen(process.env.port ?? 3000, '0.0.0.0');
 }
-bootstrap();
+void bootstrap();
